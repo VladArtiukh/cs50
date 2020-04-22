b28b3e3499742637681298a6a92f0a7dd97a01ea
VladArtiukh <43929222+VladArtiukh@users.noreply.github.com>
2020-04-22 17:02:59 +0300

    Create 1.cpp

---
+++ 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Enter number: ";
+    int n = 0;
+    cin >> n;
+        for(int i = 1; i<=n; i++)
+        {
+        cout << i << " ";
+        }
+        cout << " " << endl;
+        return 0;
+}
